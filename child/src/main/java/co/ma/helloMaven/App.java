22552a03c1d1e477b906768494aee35b15e2f6be
Adil BOUKROUROU <boukrourouadil@gmail.com>
2019-09-29 15:21:35 +0000

    initial import

---
+++ child/src/main/java/co/ma/helloMaven/App.java
@@ -0,0 +1,13 @@
+package co.ma.helloMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
